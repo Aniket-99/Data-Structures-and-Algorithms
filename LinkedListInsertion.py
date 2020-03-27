6fbb252e23a2727fd5f01e162d0e2679ac8076e2
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2020-03-28 00:33:07 +0530

    Linked list insertion in python

---
+++ LinkedListInsertion.py
@@ -0,0 +1,37 @@
+class Node:
+	"""docstring for Node"""
+	def __init__(self, data):
+		self.data = data
+		self.next = None
+
+class LinkedList(object):
+	"""docstring for LinkedList"""
+	def __init__(self):
+		self.head = None
+
+	def push(self, new_data):
+		new_node = Node(new_data)
+		new_node.next = self.head
+		self.head = new_node
+
+	def printList(self):
+		temp = self.head
+		while (temp):
+			print(temp.data)
+			temp = temp.next
+
+
+
+if __name__== '__main__':
+	list  = LinkedList()
+
+	list.push(2)
+	list.push(4)
+	list.push(9)
+
+	print("created linked list is: ")
+	list.printList()
+
+
+		
+		
