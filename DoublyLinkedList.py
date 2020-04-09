4e8be815df194c5649be959a0c8f92fdcc69bd13
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2020-04-09 23:15:14 +0530

    Deletion from doubly linked list

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -10,7 +10,7 @@ class DoublyLinkedList(object):
 	def __init__(self):
 		self.head = None
 
-# Inserting a node at start
+# Inserting a node at start--------
 	def Push(self, new_data):
 		new_node = Node(new_data)
 
@@ -19,6 +19,24 @@ class DoublyLinkedList(object):
 		if self.head is not None:
 			self.head.prev = new_node
 		self.head = new_node
+
+# Deleting a element in doubly linked list
+	def deleteNode(self, dele):
+		if self.head is None or dele is None: # If linked list is empty
+			return
+
+		
+
+		if self.head == dele:	# If the node to be deleted is first node
+			self.head = dele.next
+
+		if dele.next is not None:
+			dele.next.prev = dele.prev
+
+		if dele.prev is not None:
+			dele.prev.next = dele.next
+			
+
 
 	def printList(self):
 		temp = self.head
@@ -33,4 +51,7 @@ llist = DoublyLinkedList()
 
 llist.Push(10)
 llist.Push(20)
+llist.Push(5)
+llist.Push(6)
+llist.deleteNode(llist.head)
 llist.printList()
