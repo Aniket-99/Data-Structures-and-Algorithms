12c94ffd7ef97a15991c35722719b37e90da7c96
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2020-07-17 05:34:02 +0530

    Program to sort the array using heap sort

--- HeapSort.py
+++ HeapSort.py
@@ -1,55 +1,59 @@
-def heapSort(arr, n):
-	build_heap_bottom_up(arr, n)
+# Time complexity: O(nlogn)
+# n for building heap tree and logn for sorting
+# space complexity: O(1)
 
-	while n > 1:
-		maxValue = arr[1]
-		arr[1] = arr[n]
-		arr[n] = maxValue
-		n = n-1
-		restoreDown(1, arr, n)  
+def heapSort(array, size_of_array):
+	build_heap_bottom_up(array,size_of_array)
 
+	while size_of_array > 1:
+		max_val = array[1]
+		array[1] = array[size_of_array]
+		array[size_of_array] = max_val
+		size_of_array = size_of_array - 1
+		restoreDown(1,array,size_of_array)
 
-def build_heap_bottom_up(arr, n):
-	i = n//2
-	while i>=1:
-		restoreDown(i, arr, n)
-		i = i-1
+def build_heap_bottom_up(array, size_of_array):
+	index = size_of_array//2
+	while index >= 1:
+		restoreDown(index, array, size_of_array)
+		index = index - 1
 
-def restoreDown(i, arr, n):
-	k = arr[i]
-	lChild = 2 * i
-	rChild = lChild + 1
+def restoreDown(index, array, size_of_array):
+	value_at_index = array[index]
+	lchild = 2 * index
+	rchild = lchild + 1
 
-	while rChild <= n:
-		if k >= arr[rChild] and k >= arr[lChild]:
-			arr[i] = k
+	while rchild <= size_of_array:
+		if value_at_index >= array[lchild] and value_at_index >= array[rchild]:
+			array[index] = value_at_index
 			return
-		elif arr[lChild] > arr[rChild]:
-			arr[i] = arr[lChild]
-			i = lChild
 		else:
-			arr[i] = arr[rChild]
-			i = rChild
+			if array[lchild] > array[rchild]:
+				array[index] = array[lchild]
+				index = lchild
+			else:
+				array[index] = array[rchild]
+				index = rchild
 
-		lChild = 2 * i
-		rChild = lChild + 1
-
-
-	if lChild == n and k < arr[lChild]:
-		arr[i] = arr[lChild]
-		i = lChild
-	arr[i] = k
+		lchild = 2 * index
+		rchild = lchild + 1
 
 
-n = int(input("Enter the number of elements: "))
+	if lchild == size_of_array and value_at_index < array[lchild]:
+		array[index] = array[lchild]
+		index = lchild
+	array[index] = value_at_index
 
-arr = [None] * (n+1)
 
-for i in range(1,n+1):
-	arr[i] = int(input("Enter the number: "))
+size_of_array = int((input("Enter number of elements in the array: ")));
+array = [None] * (size_of_array+1)
 
-heapSort(arr, n)
+for i in range(1,size_of_array+1):
+	array[i] = int(input("Enter the elements: "))
 
-for i in range(1,n+1):
-	print(arr[i], " ", end="")
+heapSort(array, size_of_array)
+
+for i in range(1, size_of_array+1):
+	print(array[i]," " ,end=" ")
 print()
+
