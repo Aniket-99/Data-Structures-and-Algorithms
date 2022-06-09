97679c2e69f4da7a0e98ed6ac293138619598b44
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-06-10 00:24:47 +0530

    Two Sum II Leetcode solution

---
+++ Problem Solving/TwoSumII.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class TwoSumII {
+    public static void main(String[] args) {
+        int[] numbers = { 2, 7, 11, 15 };
+        int target = 9;
+
+        System.out.println(Arrays.toString(twoSum(numbers, target)));
+    }
+
+    static public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while (left <= right) {
+            int sum = numbers[left] + numbers[right];
+
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                return new int[] { left + 1, right + 1 };
+
+            }
+        }
+
+        return new int[] { left + 1, right + 1 };
+
+    }
+}
