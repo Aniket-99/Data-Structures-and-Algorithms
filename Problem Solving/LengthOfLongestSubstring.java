be0b333b3cda5fc6466d028e3d6cc65c1362a4a6
Aniket Karmakar <Aniketkarmakar89@gmail.com>
2022-06-11 01:00:51 +0530

    Length of longest substring Leetcode solution

---
+++ Problem Solving/LengthOfLongestSubstring.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class LengthOfLongestSubstring {
+    public static void main(String[] args) {
+        String s = "abcabc";
+        System.out.println(lengthOfLongestSubstring(s));
+
+    }
+
+    static public int lengthOfLongestSubstring(String s) {
+        int left = 0;
+        int right = 0;
+        int max = 0;
+
+        HashSet<Character> set = new HashSet<>();
+
+        while (right < s.length()) {
+            if (!set.contains(s.charAt(right))) {
+                set.add(s.charAt(right));
+                right++;
+                max = Math.max(set.size(), max);
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+
+        }
+
+        return max;
+
+    }
+}
